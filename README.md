4da7a0f8e49fd2d3564eba09cae8e602d428a3b4
Joshua Boothby <Mona.Lisa@example.com>
2025-03-14 20:58:01 -0500

    Day 5 Assignment: GitHub API Practice

---
+++ README.md
@@ -0,0 +1,6 @@
+1. For part one I used my notes from class to write out a get and post request with fetch just took a bit of adjusting
+
+2. For part 1 of part 2 I didn't have much trouble using my notes to fetch my user information
+   then for part 2 of 2 I used a for loop to iterate through the first 5 repos
+
+the functions will log the data to the console when the site loads
---
+++ app.js
@@ -0,0 +1,78 @@
+"use strict";
+// Part 1: 1
+async function fetchData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await response.json();
+
+    console.log("Response Data:", data);
+
+    if (data.length > 0) {
+      console.log("Title of the First Post:", data[0].title);
+    } else {
+      console.log("No posts available.");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchData();
+
+// Part 1: 2
+async function getGitPost() {
+  const options = {
+    method: "POST",
+    title: "Hello GitHub",
+    body: "This is a sample post",
+    userId: 1,
+  };
+
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts",
+      options
+    );
+    const data = await response.json();
+
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getGitPost();
+
+// Part 2: 1
+async function fetchGitHubUserInfo(username) {
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const data = await response.json();
+
+    console.log(`Username: ${data.login}`);
+    console.log(`Public Repos: ${data.public_repos}`);
+    console.log(`Profile: ${data.html_url}`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Part 2: 2
+async function listGitHubUserRepos(username) {
+  try {
+    const response = await fetch(
+      `https://api.github.com/users/${username}/repos`
+    );
+    const data = await response.json();
+
+    console.log("First five repositories:");
+    for (let i = 0; i < 5 && i < data.length; i++) {
+      console.log(`${i + 1}. ${data[i].name}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+fetchGitHubUserInfo("JoshuaBoothby");
+listGitHubUserRepos("JoshuaBoothby");
